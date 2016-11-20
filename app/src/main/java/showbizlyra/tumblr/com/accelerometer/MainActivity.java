1c59f7b1d75252a9e922818de0396924e6247cc2
showbizlyra <just.muhammadridwan@gmail.com>
2016-11-20 21:01:32 +0700

    Initial commit

---
+++ app/src/main/java/showbizlyra/tumblr/com/accelerometer/MainActivity.java
@@ -0,0 +1,78 @@
+package showbizlyra.tumblr.com.accelerometer;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager sm ;
+    private Sensor senAccel;
+    TextView tvHasil;
+
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super .onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        //cek apakah sensor tersedia
+//        sm = (SensorManager)getSystemService(getApplicationContext().SENSOR_SERVICE);
+//        senAccel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+                Toast toast = Toast.makeText(getApplicationContext(), "SHAKE SHAKE BOOM!", Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
